d0b8e9b2d08f7668fe42d8993e273177f53b4325
h3rby7 <herbielutz@gmx.net>
2024-12-06 19:51:50 +0100

    first map-reduce returns sorted cell of trace_ids to tables for these trace_ids

--- examples/Alibaba/Matlab/map_trace_sanity.m
+++
@@ -1,5 +0,0 @@
-function map_trace_sanity
-    entry_service_id = '7695b43b41732a0f15d3799c8eed2852665fe8da29fd700c383550fc16e521a3';
-
-    
-end
--- examples/Alibaba/Matlab/traceIdFilter.m
+++ examples/Alibaba/Matlab/traceIdFilter.m
@@ -6,5 +6,13 @@ function traceIdFilter(data, ~, intermKVStore, entry_service_id)
     trace_id_idx = intersect(trace_id_idx_1,trace_id_idx_2);
     
     trace_ids = data.traceid(trace_id_idx);
-    addmulti(intermKVStore, trace_ids, cell(length(trace_ids),1));
+    asCells = cell(height(trace_ids),1);
+
+    for i=1:height(trace_ids)
+        traceid = trace_ids(i);
+        related_entries = data((strcmp(data.traceid,traceid)>0),:);
+        asCells{i} = table2cell(related_entries);
+    end
+
+    addmulti(intermKVStore, trace_ids, asCells);
 end
