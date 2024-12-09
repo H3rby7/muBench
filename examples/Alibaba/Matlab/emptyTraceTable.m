ef7109ebfb8eda07a9eaa91a7e54104ee4f7eba4
h3rby7 <herbielutz@gmx.net>
2024-12-09 16:22:01 +0100

    get sanitized traces as map_reduce done. (165s in parallel for 24k traces)

--- examples/Alibaba/Matlab/emptyTraceTable.m
+++
@@ -1,5 +0,0 @@
-function [T] = emptyTraceTable()
-    [~, ~, trace_header, trace_vartypes, ~, ~] = config();
-    T = table('Size', [0 9], 'VariableTypes',trace_vartypes, 'VariableNames',trace_header);
-end
-% testme = emptyTraceTable();
--- examples/Alibaba/Matlab/traceIdReducer.m
+++ examples/Alibaba/Matlab/traceIdReducer.m
@@ -1,19 +1,34 @@
 function traceIdReducer(trace_id, intermValIter, outKVStore)
-    T = emptyTraceTable();
+    [upstream_is_user_id] = constants();
+    [~, ~, trace_header, trace_vartypes, ~, ~] = config();
+
+    % Create an empty table
+    T = table('Size', [0 9], 'VariableTypes',trace_vartypes, 'VariableNames',trace_header);
+    
+    % Append all entries we have to the table
     while hasnext(intermValIter)
         t_traces = getnext(intermValIter);
         T = [T;t_traces];
     end
+
+    % Table has no entries, no more work to do
     if height(T) == 0
         return
     end
-    if are_parents_present(T, trace_id)
-        % TODO: Add  the 'connectivity check'
-        add(outKVStore, trace_id, sortrows(T,'rpc_id','ascend'));
+
+    % Check for sane rpc_id structure in the traces
+    if not(are_parents_present(T))
+        return
     end
+
+    % Check if all mentioned ms are connected to the entrypoint
+    if not(check_ms_connectivity(T, upstream_is_user_id))
+        return
+    end
+    add(outKVStore, trace_id, sortrows(T,'rpc_id','ascend'));
 end
 
-function [pass] = are_parents_present(trace, trace_id)
+function [pass] = are_parents_present(trace)
     % as soon as one trace entry is falsy we drop the whole trace_id
     pass = true;
     
@@ -28,7 +43,7 @@ function [pass] = are_parents_present(trace, trace_id)
         last_digit = rpc_id(last_dot+1:end);
         if last_digit == '0'
             % numbering error (should never be 0)
-            % fprintf("drop trace [%s], because contains an RPC_ID with last digit '0' [%s]\n", trace_id, rpc_id);
+            % drop trace, because last digit of rpc_id is '0'
             pass=false;
             break
         end
@@ -36,10 +51,7 @@ function [pass] = are_parents_present(trace, trace_id)
         rpc_id_parent = rpc_id(1:last_dot-1);
         if isempty(find(strcmp(trace.rpc_id,rpc_id_parent),1))
             % parent doesn't exist
-            % Maybe we could make an exception for a missing the '0', as we
-            % know that service is out entry_point?
-
-            % fprintf("drop trace [%s], because parent [%s] is missing\n", trace_id, rpc_id_parent);
+            % drop trace, because parent ms rpc_id is not present
             pass=false;
             break
         end
@@ -49,4 +61,18 @@ function [pass] = are_parents_present(trace, trace_id)
         % in the trace, where it will always find itself and be happy.
         % We skip that step.
     end    
+end
+
+
+function [pass] = check_ms_connectivity(T, upstream_is_user_id)
+    % check if all entries of this trace are connected to each other
+    pass = true;
+
+    G = digraph(T.upstream_ms', T.downstream_ms', 'omitselfloops');
+    depths = distances(G,upstream_is_user_id);
+    if any(depths==inf)
+        % at least one node of the graph is infinitely far away
+        % -> meaning: not connected
+        pass = false;
+    end
 end
