1d65a678b3e26e3ebe6372ff2b3f4296679d8727
h3rby7 <herbielutz@gmx.net>
2024-12-09 20:14:11 +0100

    add check for empty idx (this is the case when a service trace only has rpc_id '0')

--- examples/Alibaba/Matlab/get_json_mubench.m
+++ examples/Alibaba/Matlab/get_json_mubench.m
@@ -1,5 +1,9 @@
 function js = get_json_mubench(rpc_id,app_trace,names_map,parallel)
 idx = find(strcmp(app_trace.rpc_id,rpc_id)>0,1);
+if isempty(idx)
+    js = "";
+    return
+end
 ms = app_trace.upstream_ms{idx};
 ms_id = find(strcmp(names_map,ms)>0,1)-1;
 js = char(34)+"s"+num2str(ms_id)+"__"+num2str(randi([0 1e5]))+char(34)+":[";    
