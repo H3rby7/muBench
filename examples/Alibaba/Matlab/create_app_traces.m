e05f7ca67a39eabdbfaa94edea09d5987f22d249
h3rby7 <herbielutz@gmx.net>
2024-12-10 16:42:54 +0100

    trace grouping warns about MS number inconsistency - todo: check

--- examples/Alibaba/Matlab/create_app_traces.m
+++ examples/Alibaba/Matlab/create_app_traces.m
@@ -15,8 +15,27 @@ function [app_traces] = create_app_traces(v_G_app,u_trace_ids_a,sanitized_traces
 
         % trace/app check
         ms = unique([app_traces{i}.upstream_ms ; app_traces{i}.downstream_ms]);
-        if (length(ms)-1 ~= v_G_app{i}.numnodes)
+        u_ms_length = length(ms)-1;
+        numnodes = v_G_app{i}.numnodes;
+        if (u_ms_length ~= numnodes)
             fprintf('Warning trace %d not consistent with app graph',i);
+            m = max([height(ms); height(v_G_app{i}.Nodes.Name)]);
+            cmp = cell(m,2);
+            ms_sorted = sort(ms);
+            for j=1:m
+                if j > length(ms_sorted)
+                    break;
+                end
+                cmp{j,1} = ms_sorted(j);
+            end
+            nodes_sorted = sort(v_G_app{i}.Nodes.Name);
+            for j=1:m
+                if j > length(nodes_sorted)
+                    break;
+                end
+                cmp{j,2} = nodes_sorted{j};
+            end
+            cmp
         end
         
     end
