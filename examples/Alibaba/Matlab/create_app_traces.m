a00009d701422fc59dec6e3ea825b846b8ddcc52
h3rby7 <herbielutz@gmx.net>
2024-12-10 17:53:13 +0100

    create_app_traces works with table directly instead of cell assignments

--- examples/Alibaba/Matlab/create_app_traces.m
+++ examples/Alibaba/Matlab/create_app_traces.m
@@ -3,22 +3,21 @@ function [app_traces] = create_app_traces(apps,sanitized_traces)
     % u_trace_ids_a service of app #i
     % Alibaba sanitized traces
     % app_traces{i} is a table that contains the subset of traces of the app #i
-    u_trace_ids_a = apps.trace_ids;
-    u_services_a = apps.service_ids;
-    v_G_app = apps.graph;
 
-    app_traces=cell(length(v_G_app),1);
-    for i=1:length(v_G_app)
-        app_traces{i} = sanitized_traces(ismember(sanitized_traces.trace_id, unique(u_trace_ids_a{i})),:);
+    h_apps = height(apps);
+
+    app_traces=cell(h_apps ,1);
+    for i=1:h_apps 
+        app_traces{i} = sanitized_traces(ismember(sanitized_traces.trace_id, unique(apps.trace_ids{i})),:);
 
         % trace/app check
         ms = unique([app_traces{i}.upstream_ms ; app_traces{i}.downstream_ms]);
 
         u_ms_length = length(ms)-1;
-        numnodes = v_G_app{i}.numnodes;
+        numnodes = apps.graph{i}.numnodes;
         if (u_ms_length ~= numnodes)
             fprintf('Warning trace %d not consistent with app graph',i);
-            m = max([height(ms); height(v_G_app{i}.Nodes.Name)]);
+            m = max([height(ms); height(apps.graph{i}.Nodes.Name)]);
             cmp = cell(m,2);
             ms_sorted = sort(ms);
             for j=1:m
@@ -27,7 +26,7 @@ function [app_traces] = create_app_traces(apps,sanitized_traces)
                 end
                 cmp{j,1} = ms_sorted(j);
             end
-            nodes_sorted = sort(v_G_app{i}.Nodes.Name);
+            nodes_sorted = sort(apps.graph{i}.Nodes.Name);
             for j=1:m
                 if j > length(nodes_sorted)
                     break;
