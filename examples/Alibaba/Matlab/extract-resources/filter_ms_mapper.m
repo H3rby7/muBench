5e5555db64b5e801433e9ab50a36bebf4171a30f
h3rby7 <herbielutz@gmx.net>
2024-12-16 17:20:41 +0100

    map reduce to relevant MS by ms_name

---
+++ examples/Alibaba/Matlab/extract-resources/filter_ms_mapper.m
@@ -0,0 +1,17 @@
+function filter_ms_mapper(data, ~, intermKVStore, used_ms)
+    % Filter for the MS we are interested in and map
+    relevant_traces = data(ismember(data.ms_name, used_ms),:);
+    by_ms_name = findgroups(relevant_traces{:, 2});
+    % Split table based on first column - gender column
+    split = splitapply( @(varargin) varargin, relevant_traces , by_ms_name);
+    ms_count = height(split);
+    ms_tables = cell(ms_count, 2);
+
+    vars = data.Properties.VariableNames;
+    for i=1:ms_count
+        t = table(split{i, :}, VariableNames=vars);
+        ms_tables{i,1} = t.ms_name{1};
+        ms_tables{i,2} = t;
+    end
+    addmulti(intermKVStore, vertcat(ms_tables(:,1)), ms_tables(:,2));
+end
---
+++ examples/Alibaba/Matlab/extract-resources/filter_ms_reducer.m
@@ -0,0 +1,19 @@
+function filter_ms_reducer(ms_name, intermValIter, outKVStore)
+    [~, ~, ~, ~, trace_selected_cols, trace_selected_vartypes, ~] = config();
+
+    % Create an empty table
+    T = table('Size', [0 6], 'VariableTypes',trace_selected_vartypes, 'VariableNames',trace_selected_cols);
+    
+    % Append all entries we have to the table
+    while hasnext(intermValIter)
+        t_traces = getnext(intermValIter);
+        T = [T;t_traces];
+    end
+
+    % Table has no entries, no more work to do
+    if height(T) == 0
+        return
+    end
+
+    add(outKVStore, ms_name, sortrows(T,'timestamp','ascend'));
+end
